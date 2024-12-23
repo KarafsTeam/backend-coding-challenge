c070d2d70a10f740eac69e414f2050ef2d8be310
Mohammad Hosein Mousavi <biomousavi@gmail.com>
2024-12-23 22:51:06 +0330

    chore: remove unused imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { validate } from './common/env.validation';
 
 @Module({
   imports: [ConfigModule.forRoot({ validate, isGlobal: true })],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}

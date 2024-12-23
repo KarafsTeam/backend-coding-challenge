b4525c88cecd9992cf96d3f0400e4f5f3983126a
Mohammad Hosein Mousavi <biomousavi@gmail.com>
2024-12-23 21:33:21 +0330

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { swaggerConfig } from './common/swagger';
+import { SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // swagger config
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('/docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();

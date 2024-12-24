d3c32eb29db75d8c5218966e2dc754dba71813e9
Mohammad Hosein Mousavi <biomousavi@gmail.com>
2024-12-24 10:08:38 +0330

    feat: add mongoose module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,29 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { validate } from './common/env.validation';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { EnvironmentVariables, validate } from './common/env.validation';
 import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ConfigModule.forRoot({ validate, isGlobal: true }), AuthModule, UserModule],
+  imports: [
+    ConfigModule.forRoot({ validate, isGlobal: true }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService<EnvironmentVariables>) => ({
+        dbName: configService.get('MONGO_DATABASE'),
+        uri: `mongodb://${configService.get('MONGO_HOST')}:${configService.get('MONGO_PORT')}`,
+        auth: {
+          username: configService.get('MONGO_USERNAME'),
+          password: configService.get('MONGO_PASSWORD'),
+        },
+      }),
+    }),
+    AuthModule,
+    UserModule,
+  ],
   controllers: [AppController],
 })
 export class AppModule {}

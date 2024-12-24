00831c6cd86ad6df06023d69d77f44c187249423
Mohammad Hosein Mousavi <biomousavi@gmail.com>
2024-12-24 14:51:48 +0330

    feat: export user repo

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,12 +2,14 @@ import { forwardRef, Module } from '@nestjs/common';
 import { UserSchema, User } from './user.schema';
 import { AuthModule } from 'src/auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserRepository } from './user.repository';
 
 @Module({
   imports: [
     forwardRef(() => AuthModule),
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
-  exports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
+  exports: [UserRepository, MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
+  providers: [UserRepository],
 })
 export class UserModule {}

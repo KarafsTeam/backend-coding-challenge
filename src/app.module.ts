350b6fbe04a40b969677d347074cae307c311895
Mohammad Hosein Mousavi <biomousavi@gmail.com>
2024-12-24 09:35:36 +0330

    feat: init user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { ConfigModule } from '@nestjs/config';
 import { validate } from './common/env.validation';
 import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ validate, isGlobal: true }), AuthModule],
+  imports: [ConfigModule.forRoot({ validate, isGlobal: true }), AuthModule, UserModule],
   controllers: [AppController],
 })
 export class AppModule {}

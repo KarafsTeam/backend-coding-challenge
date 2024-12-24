1ab3cb57c62e2d699e3e0f313d9b0ba81d52dceb
Mohammad Hosein Mousavi <biomousavi@gmail.com>
2024-12-24 11:04:21 +0330

    feat: add User to mongoose

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,4 +1,13 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
+import { UserSchema, User } from './user.schema';
+import { AuthModule } from 'src/auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
-@Module({})
+@Module({
+  imports: [
+    forwardRef(() => AuthModule),
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
+  exports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
+})
 export class UserModule {}

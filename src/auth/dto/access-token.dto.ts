b432b8b701f7f21139afd3ddff7079364bb83655
Mohammad Hosein Mousavi <biomousavi@gmail.com>
2024-12-24 22:43:34 +0330

    chore: add api prop

--- src/auth/dto/access-token.dto.ts
+++ src/auth/dto/access-token.dto.ts
@@ -1,8 +1,10 @@
 import { Transform } from 'class-transformer';
 import { IsNotEmpty, IsString } from 'class-validator';
 import { trimEmptySpaces } from 'src/common/utils';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class GetNewTokenRequestDto {
+  @ApiProperty({ description: 'Refresh token to obtain a new access token and refresh token' })
   @Transform(({ value }) => trimEmptySpaces(value))
   @IsString()
   @IsNotEmpty()

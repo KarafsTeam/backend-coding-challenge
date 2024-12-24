ac84ea591bea195f8f5e81712fef379b8ee954fb
Mohammad Hosein Mousavi <biomousavi@gmail.com>
2024-12-24 11:16:57 +0330

    feat: add signup dto

---
+++ src/auth/dto/signup.dto.ts
@@ -0,0 +1,42 @@
+import { PickType } from '@nestjs/mapped-types';
+import { Transform } from 'class-transformer';
+import { IsEmail, IsNotEmpty, IsString, MaxLength, IsEnum } from 'class-validator';
+import { trimEmptySpaces } from 'src/common/utils';
+import { User } from 'src/user/user.schema';
+import { ApiProperty } from '@nestjs/swagger';
+import { UserRole } from 'src/user/user.schema';
+
+export class SignUpDTO extends PickType(User, ['email', 'password', 'role']) {
+  @ApiProperty({
+    description: 'User email address',
+    example: 'user@example.com',
+    maxLength: 128,
+  })
+  @MaxLength(128)
+  @IsEmail({})
+  @Transform(({ value }) => trimEmptySpaces(value))
+  @IsString()
+  @IsNotEmpty()
+  email: string;
+
+  @ApiProperty({
+    description: 'User password',
+    example: 'Password123',
+    maxLength: 30,
+  })
+  @MaxLength(30)
+  @Transform(({ value }) => trimEmptySpaces(value))
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+
+  @ApiProperty({
+    description: 'User role',
+    enum: UserRole,
+    example: UserRole.USER,
+    default: UserRole.USER,
+  })
+  @IsEnum(UserRole)
+  @IsNotEmpty()
+  role: UserRole = UserRole.USER;
+}

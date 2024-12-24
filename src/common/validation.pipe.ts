5bb70f4b775cdd520575f3bd9348a5cefe1e1f1b
Mohammad Hosein Mousavi <biomousavi@gmail.com>
2024-12-24 16:32:18 +0330

    refactor: add all errors

--- src/common/validation.pipe.ts
+++ src/common/validation.pipe.ts
@@ -4,15 +4,10 @@ const GlobalValidationPipe = new ValidationPipe({
   whitelist: true,
   stopAtFirstError: true,
   exceptionFactory: (errors) => {
-    const transformedErrors = errors.map((error) => ({
-      field: error.property,
-      message: Object.values(error.constraints)[0],
-    }));
-
     return new BadRequestException({
+      errors,
       status: 'error',
       message: 'Validation failed',
-      errors: transformedErrors,
     });
   },
 });

147987528933f662acb220e437f8f5372d0d36ba
Mohammad Hosein Mousavi <biomousavi@gmail.com>
2024-12-25 17:22:54 +0330

    feat: handle path with slash

--- src/common/controllers/tracking.controller.ts
+++ src/common/controllers/tracking.controller.ts
@@ -2,5 +2,6 @@ import { Controller } from '@nestjs/common';
 
 // We'll use a utility function to create the base path
 export function TrackingController(path: string): ClassDecorator {
-  return Controller(`tracking/${path}`);
+  const formattedPath = path.startsWith('/') ? path.slice(1) : path;
+  return Controller(`tracking/${formattedPath}`);
 }

fae7510368c6be6fa75aed229adb4b1163ef46b4
Mohammad Hosein Mousavi <biomousavi@gmail.com>
2024-12-26 17:02:14 +0330

    feat: add api property for swagger

--- src/water-tracking/schemas/water-goal.schema.ts
+++ src/water-tracking/schemas/water-goal.schema.ts
@@ -2,15 +2,28 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Types } from 'mongoose';
 import { AbstractDocument } from 'src/common/database/abstract.schema';
 import { User } from 'src/user/user.schema';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Schema({ timestamps: true, versionKey: false })
 export class WaterGoal extends AbstractDocument {
+  @ApiProperty({
+    description: 'Reference to the user who set the water goal',
+    type: String,
+  })
   @Prop({ type: Types.ObjectId, ref: User.name, required: true })
   user: Types.ObjectId | User;
 
+  @ApiProperty({
+    description: 'Daily water intake goal in milliliters',
+    example: 2000,
+  })
   @Prop({ required: true, default: 2000 })
   dailyGoal!: number;
 
+  @ApiProperty({
+    description: 'Indicates if the water goal is currently active',
+    example: true,
+  })
   @Prop({ required: true, default: true })
   isActive!: boolean;
 }

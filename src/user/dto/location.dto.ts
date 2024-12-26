c760aafc73f46df788341abc7b55916c44f13d06
Mohammad Hosein Mousavi <biomousavi@gmail.com>
2024-12-26 22:04:30 +0330

    feat: add user location dto

---
+++ src/user/dto/location.dto.ts
@@ -0,0 +1,9 @@
+import { IsBoolean, IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class PathLocationTrackingDto {
+  @ApiProperty({ description: 'Enable or disable location tracking' })
+  @IsBoolean()
+  @IsNotEmpty()
+  enabled: boolean;
+}

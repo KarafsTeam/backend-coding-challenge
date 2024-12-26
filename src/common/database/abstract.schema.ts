4f092396b74c3aa977737d47d08288bc02283f22
Mohammad Hosein Mousavi <biomousavi@gmail.com>
2024-12-26 17:15:25 +0330

    feat: add timestamp filed types

--- src/common/database/abstract.schema.ts
+++ src/common/database/abstract.schema.ts
@@ -1,8 +1,27 @@
 import { Prop, Schema } from '@nestjs/mongoose';
 import { SchemaTypes, Types } from 'mongoose';
+import { ApiProperty } from '@nestjs/swagger';
 
-@Schema()
+@Schema({ timestamps: true })
 export class AbstractDocument {
+  @ApiProperty({
+    description: 'Unique identifier for the document',
+    type: String,
+  })
   @Prop({ type: SchemaTypes.ObjectId })
   _id: Types.ObjectId;
+
+  @ApiProperty({
+    description: 'The date and time when the document was created',
+    type: Date,
+  })
+  @Prop({ type: Date, default: Date.now, required: false })
+  createdAt: Date;
+
+  @ApiProperty({
+    description: 'The date and time when the document was last updated',
+    type: Date,
+  })
+  @Prop({ type: Date, default: Date.now, required: false })
+  updatedAt: Date;
 }

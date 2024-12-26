07221b9822ab861987d108bfcd7d454b1aa9aebf
Mohammad Hosein Mousavi <biomousavi@gmail.com>
2024-12-26 21:21:17 +0330

    chore: add swagger for streak

--- src/water-tracking/schemas/water-streak.schema.ts
+++ src/water-tracking/schemas/water-streak.schema.ts
@@ -3,18 +3,23 @@ import { Types } from 'mongoose';
 import { AbstractDocument } from 'src/common/database/abstract.schema';
 import { User } from 'src/user/user.schema';
 import { WaterGoal } from './water-goal.schema';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Schema({ timestamps: true, versionKey: false })
 export class WaterStreak extends AbstractDocument {
+  @ApiProperty({ type: String, description: 'User ID or User object' })
   @Prop({ type: Types.ObjectId, ref: User.name, required: true })
   user!: Types.ObjectId | User;
 
+  @ApiProperty({ type: Number, description: 'Current streak count', default: 0 })
   @Prop({ required: true, default: 0 })
   currentStreak!: number;
 
+  @ApiProperty({ type: String, description: 'Water goal ID or WaterGoal object' })
   @Prop({ type: Types.ObjectId, ref: WaterGoal.name, required: true })
   goal!: Types.ObjectId | WaterGoal;
 
+  @ApiProperty({ type: Boolean, description: 'Is the streak active', default: true })
   @Prop({ required: true, default: true })
   isActive!: boolean;
 }

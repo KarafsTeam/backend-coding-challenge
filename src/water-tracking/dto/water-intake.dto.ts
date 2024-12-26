8c96dfd20b3dc66841c2edc6b389fe50646311ae
Mohammad Hosein Mousavi <biomousavi@gmail.com>
2024-12-26 21:18:28 +0330

    feat: add water intake response

--- src/water-tracking/dto/water-intake.dto.ts
+++ src/water-tracking/dto/water-intake.dto.ts
@@ -1,9 +1,18 @@
 import { IsNotEmpty, IsNumber } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
+import { WaterIntake, WaterStreak } from '../schemas';
 
-export class PostWaterIntakeDto {
+export class PostWaterIntakeRequestDto {
   @ApiProperty({ description: 'Water intake in milliliters', example: 2000 })
   @IsNumber()
   @IsNotEmpty()
   amount: number;
+}
+
+export class TrackWaterIntakeResponseDto {
+  @ApiProperty({ type: WaterIntake })
+  intake: WaterIntake;
+
+  @ApiProperty({ type: WaterStreak })
+  streak: WaterStreak;
 }

edbb045f1e256672fcb9b8482467894007e1d798
Mohammad Hosein Mousavi <biomousavi@gmail.com>
2024-12-28 23:33:07 +0330

    feat: add kafka consumer configuration

--- src/main.ts
+++ src/main.ts
@@ -8,12 +8,26 @@ import { swaggerConfig } from './common/swagger';
 import { EnvironmentVariables } from './common/env.validation';
 import { GlobalValidationPipe } from './common/validation.pipe';
 import { AllExceptionsFilter } from './common/exception.filter';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { KAFKA_GROUP_ID } from './common/constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Get env config
   const configService: ConfigService<EnvironmentVariables> = app.get(ConfigService);
+
+  // Connect to Kafka as Consumer
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.KAFKA,
+    options: {
+      consumer: { groupId: KAFKA_GROUP_ID },
+      client: { brokers: [`${configService.get('KAFKA_HOST')}:${configService.get('KAFKA_PORT')}`] },
+    },
+  });
+
+  // Start Transporter
+  await app.startAllMicroservices();
 
   // Apply the exception filter globally
   app.useGlobalFilters(new AllExceptionsFilter());

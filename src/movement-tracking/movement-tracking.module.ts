722b705c68100dfbb77034b3d7e8211cc3d13923
Mohammad Hosein Mousavi <biomousavi@gmail.com>
2024-12-28 23:40:49 +0330

    chore: add authModule dependency

--- src/movement-tracking/movement-tracking.module.ts
+++ src/movement-tracking/movement-tracking.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MovementTrackingService } from './movement-tracking.service';
 import { MovementTrackingController } from './movement-tracking.controller';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
+  imports: [AuthModule],
   providers: [MovementTrackingService],
   controllers: [MovementTrackingController],
 })

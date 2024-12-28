7db5a5f785704eac35d95b7e44518df247277f00
Mohammad Hosein Mousavi <biomousavi@gmail.com>
2024-12-29 00:47:11 +0330

    docs: add movement log note

--- src/movement-tracking/movement-tracking.service.ts
+++ src/movement-tracking/movement-tracking.service.ts
@@ -11,9 +11,7 @@ export class MovementTrackingService {
   constructor(
     private userRepo: UserRepository,
     @Inject('KAFKA_SERVICE') private readonly kafkaClient: ClientKafka,
-  ) {
-    // console.table(kafkaClient);
-  }
+  ) {}
 
   async onModuleInit() {}
 
@@ -22,6 +20,11 @@ export class MovementTrackingService {
   }
 
   async logLocation(userId: Types.ObjectId, topic: string, location: PostGpsLocationDto) {
+    // TODO: definitely not the right way to check if location tracking is enabled
+    // cuz it adds a lot of overhead to each request
+    // it's better add location status to user token and create guard for it,
+    // or using a in-memory db and add user location status to it
+
     const isEnabled = await this.getUserLocationTrackingStatus();
 
     if (isEnabled) throw new BadRequestException('Location tracking is disabled for this user');

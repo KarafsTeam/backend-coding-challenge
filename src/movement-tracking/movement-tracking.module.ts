268ff0e0937cb9a2fed6122e5312dc3b1acb5fa7
Mohammad Hosein Mousavi <biomousavi@gmail.com>
2024-12-29 00:24:24 +0330

    feat: add user module dependency

--- src/movement-tracking/movement-tracking.module.ts
+++ src/movement-tracking/movement-tracking.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { MovementTrackingService } from './movement-tracking.service';
 import { MovementTrackingController } from './movement-tracking.controller';
 import { AuthModule } from 'src/auth/auth.module';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, UserModule],
   providers: [MovementTrackingService],
   controllers: [MovementTrackingController],
 })

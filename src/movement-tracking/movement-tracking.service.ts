4549b8eda1c2f196a046012e5832cd290b8c825c
Mohammad Hosein Mousavi <biomousavi@gmail.com>
2024-12-29 01:59:27 +0330

    feat: show traveled distance

--- src/movement-tracking/movement-tracking.service.ts
+++ src/movement-tracking/movement-tracking.service.ts
@@ -1,15 +1,17 @@
 import { Types } from 'mongoose';
+import { getDistance } from 'geolib';
 import { lastValueFrom } from 'rxjs';
 import { Injectable, Inject, BadRequestException } from '@nestjs/common';
 import { ClientKafka } from '@nestjs/microservices';
 import { UserRepository } from 'src/user/user.repository';
 import { PostGpsLocationDto } from './dto/location-log.dto';
+
+import { GpsLocationRepository } from './repository/gps-location.repository';
 @Injectable()
 export class MovementTrackingService {
-  // public kafkaClients: ClientKafka;
-
   constructor(
     private userRepo: UserRepository,
+    private gpsLocationRepo: GpsLocationRepository,
     @Inject('KAFKA_SERVICE') private readonly kafkaClient: ClientKafka,
   ) {}
 
@@ -30,5 +32,36 @@ export class MovementTrackingService {
     if (isEnabled) throw new BadRequestException('Location tracking is disabled for this user');
 
     await lastValueFrom(this.kafkaClient.emit(topic, { userId, location }));
+  }
+
+  async handleLocationData(payload: { location: PostGpsLocationDto; userId: Types.ObjectId }) {
+    const { location, userId } = payload;
+
+    // Save the GPS location data
+    await this.gpsLocationRepo.create({
+      user: userId,
+      latitude: location.latitude,
+      longitude: location.longitude,
+      timestamp: new Date(location.timestamp),
+    });
+
+    // Calculate the total distance traveled in the last hour
+    const oneHourAgo = new Date();
+    oneHourAgo.setHours(oneHourAgo.getHours() - 1);
+
+    const locations = await this.gpsLocationRepo.find({
+      user: userId,
+      timestamp: { $gte: oneHourAgo },
+    });
+
+    let totalDistance = 0;
+    for (let i = 1; i < locations.length; i++) {
+      totalDistance += getDistance(
+        { latitude: locations[i - 1].latitude, longitude: locations[i - 1].longitude },
+        { latitude: locations[i].latitude, longitude: locations[i].longitude },
+      );
+    }
+
+    console.log(`Total distance traveled in the last hour: ${totalDistance} meters`);
   }
 }
